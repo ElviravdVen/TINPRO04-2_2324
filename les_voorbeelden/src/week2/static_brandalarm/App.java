c527658d2028073615850edeb615548501667e6c
Matej Skelo <skelm@hr.nl>
2023-11-24 16:23:48 +0100

    Static democode toegevoegd (brandalarm)

---
+++ les_voorbeelden/src/week2/static_brandalarm/App.java
@@ -0,0 +1,23 @@
+/**
+ * Matej Skelo (m.skelo@hr.nl) - 2023/11/24
+ * Spelen met static
+ */
+
+public class App {
+    public static void main(String[] args) {
+        Appartement[] flatgebouw = {
+            new Appartement(1),
+            new Appartement(2),
+            new Appartement(3)
+        };
+
+         flatgebouw[0].toggleBrandalarm();
+        // Haal de comments in Appartement.java weg om de volgende regel uit te voeren
+         // Appartement.toggleBrandalarm();
+
+        for(int i = 0; i<flatgebouw.length; i++) {
+            System.out.println("Het brandalarm van huisnummer " + flatgebouw[i].getHuisnummer() + " is " + flatgebouw[i].brandalarm);
+        }
+
+    }   
+}
---
+++ les_voorbeelden/src/week2/static_brandalarm/Appartement.java
@@ -0,0 +1,20 @@
+public class Appartement {
+    /************** FIELDS **************/
+    private int huisnummer;
+    public /*static*/ boolean brandalarm = false;
+    
+    /************** CONSTRUCTOR(S) **************/
+    public Appartement(int huisnummer) {
+        this.huisnummer = huisnummer;
+    }
+
+    /************** GET/SET METHODS **************/
+    public int getHuisnummer() {
+        return this.huisnummer;
+    }
+
+    /************** ANDERE METHODS **************/
+    public /*static*/ void toggleBrandalarm() {
+        brandalarm = !brandalarm;
+    }
+}
