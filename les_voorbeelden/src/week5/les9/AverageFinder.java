58840f0d717c44c33e18ae7f38d9452ffa95fed4
Elvira van der Ven <sitde@hr.nl>
2023-12-10 16:32:57 +0100

    voorbeeld les 9

--- les_voorbeelden/src/week5/les9/AverageFinder.java
+++ les_voorbeelden/src/week5/les9/AverageFinder.java
@@ -1,8 +1,26 @@
 /* *****************************
- * Author: Elvira van der Ven  *                                                   
+ * Author: Elvira van der Ven  *
  * Date:   10/12/2023          *                                                                                          *
  *******************************/
 
-package week5.les9; 
+package week5.les9;
+
 public class AverageFinder {
+    public static void main(String[] args) {
+        System.out.println("Average finder v0.1");
+        System.out.println("Running with arguments:");
+        for(String s : args){
+            System.out.println(s);
+        }
+        double avg = findAverage(args);
+        System.out.println("The average is " + avg);
+    }
+
+    private static double findAverage(String[] input) {
+        double result = 0;
+        for (String s : input) {
+            result += Integer.parseInt(s);
+        }
+        return result;
+    }
 }
