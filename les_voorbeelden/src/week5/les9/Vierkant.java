30ecfaa0bc9cb6251c05e0e7b56f05960750e4f4
Elvira van der Ven <sitde@hr.nl>
2023-12-11 06:43:12 +0100

    LEs voorbeeld voor code blokken toegevoegd

---
+++ les_voorbeelden/src/week5/les9/Vierkant.java
@@ -0,0 +1,18 @@
+/* *****************************
+ * Author: Elvira van der Ven  *
+ * Date:   11/12/2023          *                                                                                          *
+ *******************************/
+
+package week5.les9;
+
+public class Vierkant extends Figuur{
+
+    protected double berekenOppervlakte() {
+        return super.berekenOppervlakte();
+    }
+
+
+    protected double berekenOmtrek() {
+        return super.berekenOmtrek();
+    }
+}
